caa71b4161aae84272a19c7a4a2bfa9a2be8a7f3
Sandy Luo <sandy.lo@spring-trees.com>
2020-01-02 12:06:27 +0800

    issue

---
+++ assets/ytapi.js
@@ -0,0 +1,41 @@
+class YtApi {
+    constructor() {
+        this.videoList = {};
+        this.init()
+    }
+    init() {
+        var tag = document.createElement('script');
+        tag.src = "https://www.youtube.com/iframe_api";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    }
+    ready() {
+        var _self = this;
+        Object.keys(_self.videoList).map(function (objectKey) {
+            var ytvideo = _self.videoList[objectKey];
+            ytvideo.video = new YT.Player(ytvideo.element, {
+                height: ytvideo.height,
+                width: ytvideo.width,
+                videoId: ytvideo.videoId,
+                host: 'https://www.youtube.com',
+            });
+        });
+    }
+    addVideo(options) {
+        var _self = this;
+        _self.videoList[options.name] = {
+            element: options.element,
+            height: options.height || '600',
+            width: options.width || '900',
+            videoId: options.videoId,
+        }
+    }
+    playVideo(name) {
+        var _self = this;
+        _self.videoList[name].video.playVideo();
+    }
+    stopVideo(name) {
+        var _self = this;
+        _self.videoList[name].video.stopVideo();
+    }
+}
