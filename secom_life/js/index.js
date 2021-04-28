a8748ec02e2e00572da7065dcc9484c107cde251
Sandy Luo <sandy.lo@spring-trees.com>
2021-04-28 11:27:02 +0800

    issue

--- secom_life/js/index.js
+++ secom_life/js/index.js
@@ -1,1 +1,1 @@
-"use strict";var index_view=new Vue({el:"#app",data:{salesTab:1},mounted:function(){$("#info-slide").slick({fade:!0,dots:!0}),$("#message-slide").slick({slidesToShow:3,autoplay:!0,autoplaySpeed:2e3,responsive:[{breakpoint:768,settings:{slidesToShow:1}}]}),$("#brand-slide").slick({arrows:!1,slidesToShow:5,autoplay:!0,autoplaySpeed:5e3,responsive:[{breakpoint:768,settings:{slidesToShow:2}}]})}});
+"use strict";var index_view=new Vue({el:"#app",data:{},methods:{}});
